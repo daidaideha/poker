15786e90da9b145bd4d290c9242c5fc8ab89b0e9
林玉玲 <382158690@qq.com>
2019-03-30 12:57:12 +0800

    Initial commit

---
+++ src/renderer/components/Card.vue
@@ -0,0 +1,311 @@
+<template>
+    <div class="dropdown">
+        <button id="dropdown-open" class="card">{{this.name}}</button>
+        <div id="dropdown-content" class="dropdown-content">
+            <button @click="onClick" class="dropdown-content-item-red" v-for="card in cards">{{card.name}}</button>
+            <button @click="onClick2" class="dropdown-content-item-black" v-for="card in cards2">{{card.name}}</button>
+            <button @click="onClick3" class="dropdown-content-item-rebeccapurple" v-for="card in cards3">{{card.name}}
+            </button>
+            <button @click="onClick4" class="dropdown-content-item-blue" v-for="card in cards4">{{card.name}}</button>
+            <button @click="onClick5" class="dropdown-content-item" v-for="card in cards5">{{card.name}}</button>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: ['index', 'name', 'type'],
+    data () {
+      return {
+        cards: [{
+          name: '♥️A',
+          value: 514
+        }, {
+          name: '♥️2',
+          value: 502
+        }, {
+          name: '♥️3',
+          value: 503
+        }, {
+          name: '♥️4',
+          value: 504
+        }, {
+          name: '♥️5',
+          value: 505
+        }, {
+          name: '♥️6',
+          value: 506
+        }, {
+          name: '♥️7',
+          value: 507
+        }, {
+          name: '♥️8',
+          value: 508
+        }, {
+          name: '♥️9',
+          value: 509
+        }, {
+          name: '♥️10',
+          value: 510
+        }, {
+          name: '♥️J',
+          value: 511
+        }, {
+          name: '♥️Q',
+          value: 512
+        }, {
+          name: '♥️K',
+          value: 513
+        }
+        ],
+        cards2: [{
+          name: '♠️A',
+          value: 414
+        }, {
+          name: '♠️2',
+          value: 402
+        }, {
+          name: '♠️3',
+          value: 403
+        }, {
+          name: '♠️4',
+          value: 404
+        }, {
+          name: '♠️5',
+          value: 405
+        }, {
+          name: '♠️6',
+          value: 406
+        }, {
+          name: '♠️7',
+          value: 407
+        }, {
+          name: '♠️8',
+          value: 408
+        }, {
+          name: '♠️9',
+          value: 409
+        }, {
+          name: '♠️10',
+          value: 410
+        }, {
+          name: '♠️J',
+          value: 411
+        }, {
+          name: '♠️Q',
+          value: 412
+        }, {
+          name: '♠️K',
+          value: 413
+        }
+        ],
+        cards3: [{
+          name: '♦️A',
+          value: 314
+        }, {
+          name: '♦️2',
+          value: 302
+        }, {
+          name: '♦️3',
+          value: 303
+        }, {
+          name: '♦️4',
+          value: 304
+        }, {
+          name: '♦️5',
+          value: 305
+        }, {
+          name: '♦️6',
+          value: 306
+        }, {
+          name: '♦️7',
+          value: 307
+        }, {
+          name: '♦️8',
+          value: 308
+        }, {
+          name: '♦️9',
+          value: 309
+        }, {
+          name: '♦️10',
+          value: 310
+        }, {
+          name: '♦️J',
+          value: 311
+        }, {
+          name: '♦️Q',
+          value: 312
+        }, {
+          name: '♦️K',
+          value: 313
+        }],
+        cards4: [{
+          name: '♣️A',
+          value: 214
+        }, {
+          name: '♣️2',
+          value: 202
+        }, {
+          name: '♣️3',
+          value: 203
+        }, {
+          name: '♣️4',
+          value: 204
+        }, {
+          name: '♣️5',
+          value: 205
+        }, {
+          name: '♣️6',
+          value: 206
+        }, {
+          name: '♣️7',
+          value: 207
+        }, {
+          name: '♣️8',
+          value: 208
+        }, {
+          name: '♣️9',
+          value: 209
+        }, {
+          name: '♣️10',
+          value: 210
+        }, {
+          name: '♣️J',
+          value: 211
+        }, {
+          name: '♣️Q',
+          value: 212
+        }, {
+          name: '♣️K',
+          value: 213
+        }],
+        cards5: [{
+          name: '大王',
+          value: 999
+        }, {
+          name: '小王',
+          value: 998
+        }]
+      }
+    },
+
+    methods: {
+      onClick: function (value) {
+        let result = {}
+        for (let i = 0; i < this.cards.length; i++) {
+          if (this.cards[i].name === value.target.innerText) {
+            result = this.cards[i]
+            break
+          }
+        }
+        this.$parent.onClick(result, this.index, this.type)
+      },
+      onClick2: function (value) {
+        let result = {}
+        for (let i = 0; i < this.cards2.length; i++) {
+          if (this.cards2[i].name === value.target.innerText) {
+            result = this.cards2[i]
+            break
+          }
+        }
+        this.$parent.onClick(result, this.index, this.type)
+      },
+      onClick3: function (value) {
+        let result = {}
+        for (let i = 0; i < this.cards3.length; i++) {
+          if (this.cards3[i].name === value.target.innerText) {
+            result = this.cards3[i]
+            break
+          }
+        }
+        this.$parent.onClick(result, this.index, this.type)
+      },
+      onClick4: function (value) {
+        let result = {}
+        for (let i = 0; i < this.cards4.length; i++) {
+          if (this.cards4[i].name === value.target.innerText) {
+            result = this.cards4[i]
+            break
+          }
+        }
+        this.$parent.onClick(result, this.index, this.type)
+      },
+      onClick5: function (value) {
+        let result = {}
+        for (let i = 0; i < this.cards5.length; i++) {
+          if (this.cards5[i].name === value.target.innerText) {
+            result = this.cards5[i]
+            break
+          }
+        }
+        this.$parent.onClick(result, this.index, this.type)
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+    .card {
+        font-size: 14px;
+        cursor: pointer;
+        width: 50px;
+        height: 70px;
+    }
+
+    .dropdown {
+        position: relative;
+        display: inline-block;
+    }
+
+    .dropdown-content {
+        z-index: 10000;
+        display: none;
+        position: absolute;
+        background-color: #f9f9f9;
+        min-width: 340px;
+        box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+    }
+
+    .dropdown-content-item {
+        width: 55px;
+        height: 30px;
+    }
+
+    .dropdown-content-item-blue {
+        width: 55px;
+        height: 30px;
+        background: darkblue;
+        color: white;
+    }
+
+    .dropdown-content-item-rebeccapurple {
+        width: 55px;
+        height: 30px;
+        background: rebeccapurple;
+        color: white;
+    }
+
+    .dropdown-content-item-red {
+        width: 55px;
+        height: 30px;
+        background: red;
+        color: white;
+    }
+
+    .dropdown-content-item-black {
+        width: 55px;
+        height: 30px;
+        background: black;
+        color: white;
+    }
+
+    #dropdown-content:hover {
+        display: block;
+    }
+
+    #dropdown-open:focus + #dropdown-content {
+        display: block;
+    }
+
+</style>
---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,845 @@
+<template>
+    <div id="wrapper">
+        <main>
+            <!-- 其他玩家 -->
+            <div style="display: inline-flex; width: 100%;">
+                <div>
+                    <div>左方游戏区域</div>
+                    <div>
+                        <div>
+                            <card :index="index" :name="card.name" :type="1" :key="index"
+                                  v-for="(card, index) in cards">
+                                {{card.name}}
+                            </card>
+                        </div>
+                        <div>
+                            <card :index="index" :name="card.name" :type="2" :key="index"
+                                  v-for="(card, index) in cards2">
+                                {{card.name}}
+                            </card>
+                        </div>
+                        <div>
+                            <card :index="index" :name="card.name" :type="3" :key="index"
+                                  v-for="(card, index) in cards3">
+                                {{card.name}}
+                            </card>
+                        </div>
+                    </div>
+                    <button style="width: 80px; height: 30px; margin-top: 30px;" @click="resetLeft">重置</button>
+                </div>
+                <!-- 右边玩家 -->
+                <div style="margin-left: 100px">
+                    <div style="margin-left: 110px">右方游戏区域</div>
+                    <div>
+                        <card :index="index" :name="card.name" :type="4" :key="index" v-for="(card, index) in cards4">
+                            {{card.name}}
+                        </card>
+                    </div>
+                    <div>
+                        <card :index="index" :name="card.name" :type="5" :key="index" v-for="(card, index) in cards5">
+                            {{card.name}}
+                        </card>
+                    </div>
+                    <div>
+                        <card :index="index" :name="card.name" :type="6" :key="index" v-for="(card, index) in cards6">
+                            {{card.name}}
+                        </card>
+                    </div>
+                    <button style="width: 80px; height: 30px; margin-top: 30px;" @click="resetRight">重置</button>
+                </div>
+            </div>
+            <!-- 自己 -->
+            <div style="width: 80%; position: absolute; top:430px;">己方游戏区域</div>
+            <div style="width: 80%; position: absolute; top:450px; display: inline-flex;">
+                <div>
+                    <div>
+                        <card :index="index" :name="card.name" :type="7" :key="index" v-for="(card, index) in cards7">
+                            {{card.name}}
+                        </card>
+                    </div>
+                    <div>
+                        <card :index="index" :name="card.name" :type="8" :key="index" v-for="(card, index) in cards8">
+                            {{card.name}}
+                        </card>
+                    </div>
+                    <div>
+                        <card :index="index" :name="card.name" :type="9" :key="index" v-for="(card, index) in cards9">
+                            {{card.name}}
+                        </card>
+                    </div>
+                </div>
+                <div style="margin-left: 100px">
+                    <label>发牌区</label>
+                    <div>
+                        <card :index="index" :name="card.name" :type="10" :key="index" v-for="(card, index) in cards10">
+                            {{card.name}}
+                        </card>
+                    </div>
+                    <label>弃牌区</label>
+                    <div>
+                        <card :index="index" :name="card.name" :type="11" :key="index" v-for="(card, index) in cards11">
+                            {{card.name}}
+                        </card>
+                    </div>
+                </div>
+            </div>
+            <button @click="resetMySelf" style="width: 80px; height: 30px; position: absolute; top:730px;">重置</button>
+            <button @click="resetAll" style="width: 80px; height: 30px; position: absolute; top:800px;">重置全部</button>
+        </main>
+    </div>
+</template>
+
+<script>
+  import Card from './Card'
+
+  export default {
+    name: 'landing-page',
+    components: {Card},
+    data () {
+      return {
+        cards: [{
+          name: '',
+          value: -1,
+          position: 0
+        }, {
+          name: '',
+          value: -1,
+          position: 1
+        }, {
+          name: '',
+          value: -1,
+          position: 2
+        }],
+        cards2: [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }],
+        cards3: [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }],
+        cards4: [{
+          name: '',
+          value: -1,
+          position: 0
+        }, {
+          name: '',
+          value: -1,
+          position: 1
+        }, {
+          name: '',
+          value: -1,
+          position: 2
+        }],
+        cards5: [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }],
+        cards6: [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }],
+        cards7: [{
+          name: '',
+          value: -1,
+          position: 0
+        }, {
+          name: '',
+          value: -1,
+          position: 1
+        }, {
+          name: '',
+          value: -1,
+          position: 2
+        }],
+        cards8: [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }],
+        cards9: [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }],
+        cards10: [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }],
+        cards11: [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }]
+      }
+    },
+    methods: {
+      resetLeft () {
+        this.cards = [{
+          name: '',
+          value: -1,
+          position: 0
+        }, {
+          name: '',
+          value: -1,
+          position: 1
+        }, {
+          name: '',
+          value: -1,
+          position: 2
+        }]
+        this.cards2 = [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }]
+        this.cards3 = [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }]
+      },
+      resetRight () {
+        this.cards4 = [{
+          name: '',
+          value: -1,
+          position: 0
+        }, {
+          name: '',
+          value: -1,
+          position: 1
+        }, {
+          name: '',
+          value: -1,
+          position: 2
+        }]
+        this.cards5 = [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }]
+        this.cards6 = [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }]
+      },
+      resetMySelf () {
+        this.cards7 = [{
+          name: '',
+          value: -1,
+          position: 0
+        }, {
+          name: '',
+          value: -1,
+          position: 1
+        }, {
+          name: '',
+          value: -1,
+          position: 2
+        }]
+        this.cards8 = [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }]
+        this.cards9 = [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }]
+        this.cards10 = [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }]
+        this.cards11 = [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }]
+      },
+      resetAll () {
+        this.cards = [{
+          name: '',
+          value: -1,
+          position: 0
+        }, {
+          name: '',
+          value: -1,
+          position: 1
+        }, {
+          name: '',
+          value: -1,
+          position: 2
+        }]
+        this.cards2 = [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }]
+        this.cards3 = [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }]
+        this.cards4 = [{
+          name: '',
+          value: -1,
+          position: 0
+        }, {
+          name: '',
+          value: -1,
+          position: 1
+        }, {
+          name: '',
+          value: -1,
+          position: 2
+        }]
+        this.cards5 = [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }]
+        this.cards6 = [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }]
+        this.cards7 = [{
+          name: '',
+          value: -1,
+          position: 0
+        }, {
+          name: '',
+          value: -1,
+          position: 1
+        }, {
+          name: '',
+          value: -1,
+          position: 2
+        }]
+        this.cards8 = [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }]
+        this.cards9 = [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }]
+        this.cards10 = [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }]
+        this.cards11 = [{
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }, {
+          name: '',
+          value: -1
+        }]
+      },
+      onClick: function (value, index, type) {
+        let list = []
+        switch (type) {
+          case 1:
+            list = this.cards
+            break
+          case 2:
+            list = this.cards2
+            break
+          case 3:
+            list = this.cards3
+            break
+          case 4:
+            list = this.cards4
+            break
+          case 5:
+            list = this.cards5
+            break
+          case 6:
+            list = this.cards6
+            break
+          case 7:
+            list = this.cards7
+            break
+          case 8:
+            list = this.cards8
+            break
+          case 9:
+            list = this.cards9
+            break
+          case 10:
+            list = this.cards10
+            break
+          case 11:
+            list = this.cards11
+            break
+        }
+        let card = list[index]
+        // if (card.value === value.value) return
+        card.value = value.value
+        card.name = value.name
+        list.sort((a, b) => {
+          return b.value % 100 - a.value % 100
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+        font-family: 'Source Sans Pro', sans-serif;
+    }
+
+    #wrapper {
+        background: radial-gradient(
+                ellipse at top left,
+                rgba(255, 255, 255, 1) 40%,
+                rgba(229, 229, 229, .9) 100%
+        );
+        height: 100vh;
+        padding: 60px 80px;
+        width: 100vw;
+    }
+
+    #logo {
+        height: auto;
+        margin-bottom: 20px;
+        width: 420px;
+    }
+
+    main {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    main > div {
+        flex-basis: 50%;
+    }
+
+    .left-side {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .welcome {
+        color: #555;
+        font-size: 23px;
+        margin-bottom: 10px;
+    }
+
+    .title {
+        color: #2c3e50;
+        font-size: 20px;
+        font-weight: bold;
+        margin-bottom: 6px;
+    }
+
+    .title.alt {
+        font-size: 18px;
+        margin-bottom: 10px;
+    }
+
+    .doc p {
+        color: black;
+        margin-bottom: 10px;
+    }
+
+    .doc card {
+        font-size: .8em;
+        cursor: pointer;
+        outline: none;
+        padding: 0.75em 2em;
+        border-radius: 2em;
+        display: inline-block;
+        color: #fff;
+        background-color: #4fc08d;
+        transition: all 0.15s ease;
+        box-sizing: border-box;
+        border: 1px solid #4fc08d;
+    }
+
+    .doc card.alt {
+        color: #42b983;
+        background-color: transparent;
+    }
+</style>
